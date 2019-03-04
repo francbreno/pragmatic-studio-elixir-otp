2e923cd6a31099775a124ccd8f35e076d5a3625f
Breno Soares <francbreno@gmail.com>
2019-03-04 11:08:21 -0300

    15. Slicing and Dicing with Enum - Even more concise anonymous functions

--- lib/servy/bear_controller.ex
+++ lib/servy/bear_controller.ex
@@ -8,8 +8,8 @@ defmodule Servy.BearController do
   def index(%Conv{} = conv) do
     items = 
       Wildthings.list_bears()
-      |> Enum.sort(&Bear.order_asc_by_name(&1, &2))
-      |> Enum.map(&bear_item(&1))
+      |> Enum.sort(&Bear.order_asc_by_name/2)
+      |> Enum.map(&bear_item/2)
       |> Enum.join
 
     %{ conv | status: 200, resp_body: "<il>#{items}<li>" }
