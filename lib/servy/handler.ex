57d7350c014d62695168d17adf7432283876be52
Breno Soares <francbreno@gmail.com>
2019-03-03 21:02:03 -0300

    9. Serve Static Files - Exercises Done

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -77,6 +77,13 @@ defmodule Servy.Handler do
     %{ conv | status: 200, resp_body: "Teddy, Smokey, Paddington" }
   end
 
+  def route(%{ method: "GET", path: "/bears/new" } = conv) do
+    Path.expand("../../pages", __DIR__)
+    |> Path.join("form.html")
+    |> File.read
+    |> handle_file(conv)
+  end
+
   def route(%{ method: "GET", path: "/bears/" <> id } = conv) do 
     %{ conv | status: 200, resp_body: "Bear #{id}" }
   end
@@ -90,24 +97,33 @@ defmodule Servy.Handler do
   end
 
   def route(%{ method: "GET", path: "/about" } = conv) do
-    file_path = 
-      Path.expand("../../pages", __DIR__)
-      |> Path.join("about.html")
+    Path.expand("../../pages", __DIR__)
+    |> Path.join("about.html")
+    |> File.read
+    |> handle_file(conv)
+  end
 
-    case File.read(file_path) do
-      {:ok, content} -> 
-        %{ conv | status: 200, resp_body: content }
- 
-      {:error, :enoent = reason} ->
-        %{ conv | status: 404, resp_body: "File error: #{:file.format_error(reason)}" }
-      
-      {:error, reason} ->
-        %{ conv | status: 500, resp_body: "File error: #{:file.format_error(reason)}" }
-    end
+  def route(%{ method: "GET", path: "/pages/" <> file_name } = conv) do
+    Path.expand("../../pages", __DIR__)
+    |> Path.join("#{file_name}.html")
+    |> File.read
+    |> handle_file(conv)
   end
 
   def route(%{ path: path } = conv) do
     %{ conv | status: 404, resp_body: "No #{path} here!" }
+  end
+
+  def handle_file({:ok, content}, conv) do
+    %{ conv | status: 200, resp_body: content }
+  end
+
+  def handle_file({:error, :enoent = reason}, conv) do
+    %{ conv | status: 404, resp_body: "File error: #{:file.format_error(reason)}" }
+  end
+
+  def handle_file({:error, reason}, conv) do
+    %{ conv | status: 500, resp_body: "File error: #{:file.format_error(reason)}" }
   end
 
   def format_response(conv) do
@@ -204,6 +220,26 @@ IO.puts response
 
 request = """
 GET /about HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+"""
+
+response = Servy.Handler.handle(request)
+IO.puts response
+
+request = """
+GET /bears/new HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+"""
+
+response = Servy.Handler.handle(request)
+IO.puts response
+
+request = """
+GET /pages/contact HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
